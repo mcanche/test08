85e1112c4177ee0d640bfe917c885489fc21ee3e
mcanche <mcanche@gmail.com>
2015-06-12 18:18:55 -0500

    test08
    playground - tutorial 08

---
+++ test08.playground/Contents.swift
@@ -0,0 +1,17 @@
+// +-------------+
+// | video 8     |
+// +-------------+
+
+import UIKit
+var v = UIView(frame: CGRectMake (0, 0, 200, 200))
+
+var b = IUBotton (FRame: CGRecmake (0, 0, 100, 32))
+
+
+b.setTitle ("Test Butto", forState: UIControlState.Normal);
+b.setTitleColor(UIColor.blueColor(), forState: UIControlState.normal)
+
+v.addSubview(6)
+
+
+v
